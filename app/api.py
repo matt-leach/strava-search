9e9e5c42152ea8efcf75703f3c915799ec6a14bd
Matt Leach <matt.leach25@gmail.com>
2015-09-08 22:06:24 -0700

    adding search query input

--- app/api.py
+++ app/api.py
@@ -8,18 +8,17 @@ import time
 @app.route('/api/activities', methods=['GET'])
 def html_activities():
     user_id = session['athlete']['id']
-
     # Get max and min distance parameters
     max_dist = int(request.args.get('max', 1000))
     min_dist = int(request.args.get('min', 0))
 
+    q = '%{}%'.format(request.args.get('q', ''))
     cur = g.db.cursor()
     cur.execute('select * from activities \
                 where (users_id = ?) and (distance <= ?) and (distance >= ?) \
-                order by start_time desc',
-                [user_id, max_dist, min_dist])
+                and (name like ?) order by start_time desc',
+                [user_id, max_dist, min_dist, q])
     activities = cur.fetchall()
     times = [[a[3], 0] for a in activities]
-    print type(times[0][0])
     activities = [{'name': a[1], "distance": a[2], "id": a[0]} for a in activities]
     return render_template('activities.html', activities=activities, times=times)
